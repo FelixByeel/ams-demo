a713920aa57894ad8d422e57402436962d2b1236
felix <felix_3201@hotmail.com>
2017-03-23 18:02:00 +0800

    完善添加功能

--- app/itemManagement/insertItem.php
+++ app/itemManagement/insertItem.php
@@ -19,17 +19,45 @@
     $data = json_encode($data,JSON_UNESCAPED_UNICODE);
     //echo $data;
 ?>
-
+<!--添加分类-->
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 <script type="text/javascript" src="http://libs.baidu.com/jquery/1.9.1/jquery.min.js"></script>
+<script></script>
+<style>
+    select{
+        width:199px;
+        height:25px;
+        margin-top:5px;
+        
+    }
+
+    div{
+        width:200px;
+    }
+
+</style>
 </head>
 <body>
+    
+    <div>
+        <p>请选择备货仓库：</p>
+        <label><input name = "warehouse" type = "radio" value = ""/>南山</label>
+         <label><input name = "warehouse" type = "radio" value = ""/>惠联科技</label>
+    </div>
+    <p>请选择上级分类：</p>
     <div id = "itemBox"></div>
-    <button id = "addItem2" onclick = "">添加</button>
+    <div id = "inputBox">
+        <p>请输入分类（物品）名称：</p>
+        <input id = "itemNameInput" type = "text"/>
+        <p>请输入物品数量：</p>
+        <input id = "itemCountInput" type = "text"/>
+    </div>
+    <button id = "addItem2" onclick = "addItem()">添加</button>
 </body>
 
+<!---------script---------->
 <script>
     let existSelectListCount = 0;
     let existSelectList = [];                                   //记录已经存在的select列表
@@ -40,15 +68,14 @@
 
         let itemSelectId = itemJsonObj[0].parent_id;
         let itemBoxObj = document.getElementById('itemBox');    //获取需要添加select的对象
-        let itemSelectName = -1;
+        let itemSelectName = -1;                                //第一个分类select列表Id
 
-        //调用添加分类 功能
-
+        //调用添加下一级分类 功能
         createItemSelect(itemBoxObj, itemSelectId, itemSelectName, itemJsonObj);
     }
 
-    //---------------定义添加分类方法功能--------------
-    //return 当前创建成功的select的序号即分类号
+    //---------------定义添加下一级分类方法--------------
+    //return 当前创建成功的select的id即分类号(分类号表示第一级分类，第二季分类...)
     function createItemSelect(itemBoxObj, itemSelectId, itemSelectName, itemJsonObj){
 
         let itemSelectObj = document.createElement('select');
@@ -59,7 +86,7 @@
         itemSelectObj.name = "itemSelectName_" + itemSelectName;
         
         itemOptionObj.value= 'itemOption_0';
-        itemOptionObj.text = "请选择分类";
+        itemOptionObj.text = "--无--";
 
         itemSelectObj.appendChild(itemOptionObj);
 
@@ -67,8 +94,9 @@
         for(let i = 0; i < itemJsonObj.length; i++){
 
             let itemOptionObj = document.createElement('option');
-
+            
             if(itemSelectId == itemJsonObj[i].parent_id){
+                //if(itemJsonObj[i].is_ended == 1) return false; 添加分类和添加物品切换
                itemOptionObj.value = 'itemOption_' + itemJsonObj[i].id;
                itemOptionObj.text = itemJsonObj[i].item_name;
                itemSelectObj.appendChild(itemOptionObj);
@@ -102,18 +130,9 @@
             return false;
         }
 
+        //选择分类时，下一级分类显示同步
         countSelect(will_selectedName);
-        /*
-        //通过select的id序号，判断要创建的select是否已经存在，已存在时不再重复创建
-        let i = 0;
-        while(existSelectList[i]){
-            
-            if(existSelectList[i] == will_selectedId) {
-                return false;
-            }
-            i++;
-        }
-        */
+
         //获取到当前选择项在itemJsonObj中的位置
         for(let i = 0; i < itemJsonObj.length; i++) {
             if(will_selectedId == itemJsonObj[i].id){
@@ -129,7 +148,7 @@
         }
     }
 
-    //同级分类select的name为共同上级分类select的id，同级分类在同一个select列表显示，
+    //同级分类select的name为共同上级分类select的id，同级分类在同一个select列表显示-----------------
     function  countSelect(sname){
         let selectArr = document.getElementsByTagName("select");    //获取当前已存在的select列表
         let selectName = "itemSelectName_" + sname;                 //获取要创建的select的Name
@@ -141,13 +160,25 @@
 
                 let selectId = selectArr[i].id;
 
-                $(document).ready(function(){
+                //$(document).ready(function(){
 
                     $("#" + selectId).nextAll().remove();
                     $("#" + selectId).remove();
-                });
+                //});
             }
         }
+    }
+
+    //---------------点击“添加”按钮-------------
+    function addItem(){
+        let selectArr = document.getElementsByTagName("select");
+        let str = "";
+
+        for(let i = 0; i < selectArr.length; i++){
+            str += ' + ' + selectArr[i].options[selectArr[i].selectedIndex].value.split("_")[1];
+            
+        }
+        alert(str);
     }
 </script>
 </html>
