10f422731c685236eb0e65931d7687015d663484
Felix <jadeshaw@163.com>
2017-03-28 16:29:50 +0800

    数据验证

--- app/itemManagement/insertItemService.php
+++ app/itemManagement/insertItemService.php
@@ -17,37 +17,43 @@ if (isset($_POST['itemData'])) {
     $isNumberReg = '/^[1-9]+[0-9]*]*$/';
 
     if (array_key_exists('warehouse_id', $itemData)) {
-        if (!preg_match($isNumberReg, $itemData['warehouse_id'])) {
-            die("输入数据有误!");
+        if(!empty($itemData['warehouse_id'])){
+            if (!preg_match($isNumberReg, $itemData['warehouse_id'])) {
+                die("仓库选择有误!");
+            }
+        }
+        else {
+            die("请选择一个仓库！");
         }
     }
+
     if (array_key_exists('parent_id', $itemData)) {
         if (!empty($itemData['parent_id'])) {
             if(!preg_match($isNumberReg, $itemData['parent_id'])){
-                die("输入数据有误!");
+                die("分类ID应为正整数，请确认输入是否有误!");
             }
         }
         else {
             $itemData['parent_id'] = 0;
         }
     }
-    if (array_key_exists('id', $itemData)) {
+
+    if (array_key_exists('id', $itemData) && !empty($itemData['id'])) {
         if (!preg_match($isNumberReg, $itemData['id'])) {
-            die("输入数据有误!");
+            die("分类ID应为正整数，请确认输入是否有误!");
         }
     }
-    if (array_key_exists('item_count', $itemData)) {
-        if(!empty($itemData['item_count'])){
+
+    if (array_key_exists('item_count', $itemData) && !empty($itemData['item_count'])) {
+
             if (!preg_match($isNumberReg, $itemData['item_count'])) {
-                die("输入数据有误!");
+                die("物品数量应为正整数，请确认输入是否有误!");
             }
-        }
-        else {
-            $itemData['item_count'] = 0;
-        }
-
     }
-
+    else {
+        $itemData['item_count'] = 0;
+    }
+    
     foreach ($itemData as $key => $value) {
         echo '<br />';
         echo $key.' : '.$value;
