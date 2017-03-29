beff7c092e54dc797ca4ee2120a4d604d4ad199b
Felix <jadeshaw@163.com>
2017-03-29 18:15:02 +0800

    数据库写入

--- app/itemManagement/insertItemService.php
+++ app/itemManagement/insertItemService.php
@@ -26,7 +26,7 @@ foreach ($itemData as $key => $value) {
 
 //验证提交的数据是否安全
 foreach ($itemData as $key => $value) {
-    if(checkInput($value)){
+    if (checkInput($value)) {
         die('输入的内容不能包含字符 ：' . checkInput($value));
     }
 }
@@ -42,6 +42,9 @@ if (array_key_exists('warehouse_id', $itemData)) {
         die("请选择一个仓库！");
     }
 }
+else{
+    die("请选择一个仓库！");
+}
 
 if (array_key_exists('parent_id', $itemData)) {
     if (!empty($itemData['parent_id'])) {
@@ -53,43 +56,73 @@ if (array_key_exists('parent_id', $itemData)) {
     }
 }
 
-if (array_key_exists('id', $itemData)){
-    if(!empty($itemData['id'])) {
+if (array_key_exists('id', $itemData)) {
+    if (!empty($itemData['id'])) {
         if (!preg_match($isNumberReg, $itemData['id'])) {
             die("分类ID应为正整数，请确认输入是否有误!");
         }
-    }
-    else {
+    } else {
         die("分类ID不能为空!");
     }
 }
 
 if (array_key_exists('item_count', $itemData)) {
-    if(!empty($itemData['item_count'])){
+    if (!empty($itemData['item_count'])) {
         if (!preg_match($isNumberReg, $itemData['item_count'])) {
             die("物品数量应为正整数，请确认输入是否有误!");
         }
-    }
-    else {
+    } else {
         $itemData['item_count'] = 0;
     }
 }
-
-
+else {
+    die('输入的数据有误，请重新输入！');
+}
 
 //写入数据到数据库
 $mysql = new Msqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
-
-if(3 == count($itemData) && array_key_exists('id', $itemData) && (0 != $itemData['item_count'])) {
+$tableName = 'item_t';
+if (3 == count($itemData) && array_key_exists('id', $itemData) && (0 != $itemData['item_count'])) {
     $count = $itemData['item_count'];
     $id    = $itemData['id'];
-    $result = $mysql->update('item_t', "item_count = $count", "id = $id");
-    
-}
 
+    $result = $mysql->update($tableName, "item_count = $count", "id = $id");
+
+    if ($result) {
+        echo '操作成功!';
+    } else {
+        echo '操作失败!';
+    }
+}
+else if (4 == count($itemData) && array_key_exists('item_name', $itemData) && (!empty($itemData['item_name']))) {
+    
+    $warehouseId = $itemData['warehouse_id'];
+    $parentId    = $itemData['parent_id'];
+    $itemName    = $itemData['item_name'];
+    
+    if(0 == $itemData['item_count']) {
+
+        $result = $mysql->insert($tableName, 'item_name, parent_id, warehouse_id', "$itemName, $parentId, $warehouseId");
+
+        if ($result) {
+            echo '操作成功!';
+        } else {
+            echo '操作失败!';
+        }
+    }
+    else {
+        $count = $itemData['item_count'];
+
+        $result = $mysql->insert($tableName, 'item_name, parent_id, warehouse_id, item_count', "$itemName, $parentId, $warehouseId, $count");
+    }
+}
+else {
+    die('输入的数据有误，请重新输入！');
+}
 //$result = $mysql->update();
 //测试输出
-echo '处理后：<br />';
+echo '<br />';
+echo '<br /> 处理后：<br />';
 foreach ($itemData as $key => $value) {
     echo '<br />';
     echo $key.' : '.$value;
@@ -97,3 +130,6 @@ foreach ($itemData as $key => $value) {
 
 echo '<br />';
 echo count($itemData);
+echo '<br />';
+echo $mysql->getError();
+echo $mysql->select($tableName, '*');
