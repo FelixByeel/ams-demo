f7f6efbc722c613e645d9a53fe925edbe222db0f
Felix <jadeshaw@163.com>
2017-03-31 14:26:55 +0800

    ajax获取后台数据

--- public/js/searchItem.js
+++ public/js/searchItem.js
@@ -1,11 +1,38 @@
 //页面加载初始化部分数据
 window.onload = function(){
 
-    getJSON();
+    loadWarehouse();
+    loadItem();
 }
 
-function getJSON(){
-    $.ajax{
-        
-    }
+//加载仓库信息
+function loadWarehouse(){
+    //定义查询条件
+    let warehouseSearch = {
+        "tabelName" : "warehouse_t",
+        "columnArray" : Array("warehouse_id", "warehouse_name")
+    };
+
+    //ajax()方法,返回成功调用showWarehouseCallback
+    $.ajax({
+        type:"post",
+        url : "selectTabelService.php",
+        data : sendData,
+        dataType : JSON,
+        cache: false,
+        success : getCallbackData
+    });
+}
+
+function getCallbackData(data){
+    let warehouseJSON = JSON.parse(data);
+    
+}
+
+function loadItem(){
+    let itemSearch = {
+        "tabelName" : "item_t",
+        "columnArray" : Array("*"),
+        "conditionStr" : ''
+    };
 }
