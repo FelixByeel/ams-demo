84007b71a30fe85d9b633220f4c992313de2682f
Felix <jadeshaw@163.com>
2017-04-06 11:18:42 +0800

    格式化代码，添加验证模块

--- public/js/searchItem.js
+++ public/js/searchItem.js
@@ -1,7 +1,7 @@
 //页面加载初始化，默认显示所有分类信息，实现无限分类菜单
-window.onload = function(){
+window.onload = function () {
 
-    let itemMenuDivObj  = document.getElementById('itemMenu');      //获取加载li列表的ul对象
+    let itemMenuDivObj = document.getElementById('itemMenu');      //获取加载li列表的ul对象
 
     let Global_warehouseJSON;
     let Global_itemJSON;
@@ -11,51 +11,51 @@ window.onload = function(){
 }
 
 //ajax()方法加载分类数据,返回成功调用showItemInfo()初始化分类显示
-function loadAjaxGetData(itemMenuDivObj){
+function loadAjaxGetData(itemMenuDivObj) {
 
     $.ajax({
-        type:"post",
-        url : "searchItemService.php",
-        data : {"tableName":"warehouse"},
-        dataType : "json",
+        type: "post",
+        url: "searchItemService.php",
+        data: { "tableName": "warehouse" },
+        dataType: "json",
         cache: false,
-        success : function(warehouseJSON){
+        success: function (warehouseJSON) {
             Global_warehouseJSON = warehouseJSON;
         },
-        error : function(msg,e){
-            alert( "请求的数据发生异常：" + e);
+        error: function (msg, e) {
+            alert("请求的数据发生异常：" + e);
         }
     });
 
     $.ajax({
-        type:"post",
-        url : "searchItemService.php",
-        data : {"tableName":"item"},
-        dataType : "json",
+        type: "post",
+        url: "searchItemService.php",
+        data: { "tableName": "item" },
+        dataType: "json",
         cache: false,
-        success : function(itemJSON){
+        success: function (itemJSON) {
             Global_itemJSON = itemJSON;
             showItemInfo(itemMenuDivObj, itemJSON, 0);
             loadAllEndedItems(itemMenuDivObj, itemJSON);
         },
-        error : function(msg,e){
-            alert( "请求的数据发生异常：" + e);
+        error: function (msg, e) {
+            alert("请求的数据发生异常：" + e);
         }
     });
 }
 
 //显示分类菜单
-function showItemInfo(itemMenuDivObj, itemJSON, currentSelectedId){
+function showItemInfo(itemMenuDivObj, itemJSON, currentSelectedId) {
 
     let ulFlag = 1; //当前项有子项时才创建子项的容器ul，防止创建空的<ul></ul>标签
     let itemUlObj;
 
-    for(let i = 0, j = 0; i< itemJSON.length; i++){
-        if((currentSelectedId == itemJSON[i].parent_id) && (itemJSON[i].is_ended != 1)){
+    for (let i = 0, j = 0; i < itemJSON.length; i++) {
+        if ((currentSelectedId == itemJSON[i].parent_id) && (itemJSON[i].is_ended != 1)) {
             //没有子分类的项不显示
-            if(ulFlag){
-                    itemUlObj = document.createElement("ul");
-                    ulFlag = 0;
+            if (ulFlag) {
+                itemUlObj = document.createElement("ul");
+                ulFlag = 0;
             }
 
             let itemLiObj = document.createElement("li");
@@ -63,7 +63,7 @@ function showItemInfo(itemMenuDivObj, itemJSON, currentSelectedId){
             itemLiObj.id = "itemLiId_" + itemJSON[i].id;
             itemLiObj.innerHTML = itemJSON[i].item_name;
 
-            itemLiObj.addEventListener("click", function(e){
+            itemLiObj.addEventListener("click", function (e) {
 
                 itemClicked(this, itemMenuDivObj, itemJSON);
                 e.stopPropagation();    //阻止事件冒泡，点击内层li不会触发上层li的click事件
@@ -72,12 +72,12 @@ function showItemInfo(itemMenuDivObj, itemJSON, currentSelectedId){
             itemUlObj.id = "itemUlId_" + currentSelectedId;
             itemUlObj.appendChild(itemLiObj);
         }
-        else if((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)){
+        else if ((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)) {
             showCurrentSelectedDetail(itemMenuDivObj, itemJSON, currentSelectedId);
         }
     }
 
-    if(itemUlObj){
+    if (itemUlObj) {
         itemMenuDivObj.appendChild(itemUlObj);
     }
 }
@@ -87,19 +87,19 @@ function itemClicked(currentClickLi, itemMenuDivObj, itemJSON) {
     let currentSelectedId = currentClickLi.id.split("_")[1];
 
     //检测当前点击菜单项的子菜单是否存在
-    if(checkSubItem(currentSelectedId)){
+    if (checkSubItem(currentSelectedId)) {
         itemMenuDivObj = document.getElementById("itemLiId_" + currentSelectedId);
         showItemInfo(itemMenuDivObj, itemJSON, currentSelectedId);
     }
 }
 
 //检测当前点击菜单项的子菜单是否存在
-function checkSubItem(currentSelectedId){
+function checkSubItem(currentSelectedId) {
 
     let ulListObj = document.getElementById("itemMenu").getElementsByTagName("ul");
 
-    for(let i = 0; i < ulListObj.length; i++){
-        if((ulListObj[i].id.split("_")[1]) == currentSelectedId){
+    for (let i = 0; i < ulListObj.length; i++) {
+        if ((ulListObj[i].id.split("_")[1]) == currentSelectedId) {
             let currentUl = "itemUlId_" + currentSelectedId
             $("#" + currentUl).remove();
             return false;
@@ -123,20 +123,20 @@ function showCurrentSelectedDetail(itemMenuDivObj, itemJSON, currentSelectedId) 
     trObj.insertCell(3).innerHTML = "数量";
     trObj.insertCell(4).innerHTML = "操作";
 
-    for(let i = 0, j = 0; i < itemJSON.length; i++){
-        if((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)){
+    for (let i = 0, j = 0; i < itemJSON.length; i++) {
+        if ((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)) {
             let trObj = tableObj.insertRow();
-            if(j % 2){
+            if (j % 2) {
                 trObj.className = "row_odd";
             }
-            else{
+            else {
                 trObj.className = "row_even";
             }
             trObj.insertCell(0).innerHTML = itemJSON[i].id;
             trObj.insertCell(1).innerHTML = itemJSON[i].item_name;
 
-            for(var k = 0; k < Global_warehouseJSON.length; k++){
-                if(Global_warehouseJSON[k].warehouse_id == itemJSON[i].warehouse_id){
+            for (var k = 0; k < Global_warehouseJSON.length; k++) {
+                if (Global_warehouseJSON[k].warehouse_id == itemJSON[i].warehouse_id) {
                     break;
                 }
             }
@@ -166,21 +166,21 @@ function loadAllEndedItems(itemMenuDivObj, itemJSON) {
     trObj.insertCell(3).innerHTML = "数量";
     trObj.insertCell(4).innerHTML = " ";
 
-    for(let i = 0, j = 0; i < itemJSON.length; i++){
-        if(1 == itemJSON[i].is_ended){
+    for (let i = 0, j = 0; i < itemJSON.length; i++) {
+        if (1 == itemJSON[i].is_ended) {
             let trObj = tableObj.insertRow();
-            if(j % 2){
+            if (j % 2) {
                 trObj.className = "row_odd";
             }
-            else{
+            else {
                 trObj.className = "row_even";
             }
             trObj.insertCell(0).innerHTML = itemJSON[i].id;
             trObj.insertCell(1).innerHTML = itemJSON[i].item_name;
 
-            for(var k = 0; k < Global_warehouseJSON.length; k++){
+            for (var k = 0; k < Global_warehouseJSON.length; k++) {
 
-                if(Global_warehouseJSON[k].warehouse_id == itemJSON[i].warehouse_id){
+                if (Global_warehouseJSON[k].warehouse_id == itemJSON[i].warehouse_id) {
 
                     break;
                 }
@@ -197,18 +197,18 @@ function loadAllEndedItems(itemMenuDivObj, itemJSON) {
 }
 
 //编辑操作
-function editItem(currentSelectedId){
+function editItem(currentSelectedId) {
 
     let itemIndex;
 
     $("#editBox").show();
     $("#classSelect").empty();
     $("#warehouseSelect").empty();
-    $("#codeSpan").text(currentSelectedId);
+    $("#IDSpan").text(currentSelectedId);
 
     //找到当前选择项在JSON中的位置，并保存下来
-    for(let i = 0; i < Global_itemJSON.length; i++){
-        if(Global_itemJSON[i].id == currentSelectedId){
+    for (let i = 0; i < Global_itemJSON.length; i++) {
+        if (Global_itemJSON[i].id == currentSelectedId) {
             itemIndex = i;
             break;
         }
@@ -218,40 +218,94 @@ function editItem(currentSelectedId){
     $("#countInput").val(Global_itemJSON[itemIndex].item_count);
 
     //显示可选择的所有分类，默认直接上级分类为选择状态
-    for(let i = 0; i < Global_itemJSON.length; i++){
-        if(0 == Global_itemJSON[i].is_ended){
+    for (let i = 0; i < Global_itemJSON.length; i++) {
+        if (0 == Global_itemJSON[i].is_ended) {
             let str = "<option value = '" + Global_itemJSON[i].id + "'>" + Global_itemJSON[i].item_name + "</option>";
             $("#classSelect").append(str);
         }
 
-        if(Global_itemJSON[i].id == Global_itemJSON[itemIndex].parent_id){
+        if (Global_itemJSON[i].id == Global_itemJSON[itemIndex].parent_id) {
             $("#classSelect").find("option[value = " + Global_itemJSON[i].id + "]").attr("selected", true);
         }
     }
 
-    for(let j = 0; j < Global_warehouseJSON.length; j++){
+    //显示仓库，并默认选择当前仓库
+    for (let j = 0; j < Global_warehouseJSON.length; j++) {
         let str = "<option value = '" + Global_warehouseJSON[j].warehouse_id + "'>";
         str += Global_warehouseJSON[j].warehouse_name + "</option>";
 
         $("#warehouseSelect").append(str);
 
-        if(Global_itemJSON[itemIndex].warehouse_id == Global_warehouseJSON[j].warehouse_id){
+        if (Global_itemJSON[itemIndex].warehouse_id == Global_warehouseJSON[j].warehouse_id) {
             $("#warehouseSelect").find("option[value = " + Global_warehouseJSON[j].warehouse_id + "]").attr("selected", true);
         }
     }
 }
 
 //保存
-$("#saveButton").click(function(){
+$("#saveButton").click(function () {
     alert("save");
 });
 
 //取消
-$("#cancelButton").click(function(){
+$("#cancelButton").click(function () {
     alert("cancel");
+    $("#editBox").hide();
 });
 
 //删除
-$("#delButton").click(function(){
-    alert("del");
+$("#delButton").click(function () {
+
+    let result = confirm("确认删除？");
+    let itemID = $("#IDSpan").text();
+    if(result){
+
+    }
+
 });
+
+//判断字符串是否合法，合法返回true，不合法返回false------------
+function checkInput(content, flag) {
+
+    let isNumberReg = /^[1-9]+[0-9]*]*$/;
+
+    if (1 == flag && (!isNumberReg.test(content))) {
+        alert("输入的物品数量无效，请重新输入！");
+        document.getElementById('itemCountInput').focus();
+        return false;
+    }
+    else if (0 == flag) {
+
+        let is_existC = 0;            //is_existC用来跳出双层FOR循环
+        //不接受下列字符的输入
+        let specialCharacter = new Array('~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+            '=', '+', '[', ']', '{', '}', '\\', '|', ';', ':', '\'', '\"', ',', '<', '.', '>', '?',
+            '~', '·', '！', '＠', '＃', '￥', '％', '………', '＆', '＊', '（', '）', '——', '＋', '＝',
+            '【', '】', '｛', '｝', '、', '｜', '；', '：', '’', '“', '，', '《', '。', '》', '？', ' ', '　');
+
+        for (let i = 0; i < content.length; i++) {
+            for (let j = 0; j < specialCharacter.length; j++) {
+                if (content[i] == specialCharacter[j]) {
+                    is_existC = 1;
+                    if ((' ' == specialCharacter[j]) || ('　' == specialCharacter[j])) {
+                        alert("分类名称中不能含有空格！");
+                    }
+                    else {
+                        alert("分类名称中不能含有字符：" + specialCharacter[j]);
+                    }
+
+                    document.getElementById('itemNameInput').focus();
+                    return false;
+                }
+            }
+
+            if (is_existC) return false;
+        }
+
+        if (is_existC) return false;
+        else return true;
+    }
+    else {
+        return true;
+    }
+}
