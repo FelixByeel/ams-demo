6d0d1d38b6962efef32825fb381b2740ce398e03
Felix <jadeshaw@163.com>
2017-04-07 14:01:27 +0800

    添加后台更新功能

--- app/itemManagement/updateItem.php
+++ app/itemManagement/updateItem.php
@@ -1,6 +1,31 @@
 <?php
 //更新操作
+/*
+*处理接收的JSON数据，添加到数据库
+*/
+define('APP_ROOT', dirname(dirname(__DIR__)).'/');
 
+//加载数据验证
+require_once (APP_ROOT.'include/checkInput.php');
+
+//加载数据库配置
+require_once (APP_ROOT.'include/dbConfig.php');
+require_once (APP_ROOT.'include/Msqli.class.php');
+
+//验证是否正确提交数据
+if (!isset($_POST['itemData'])) {
+    die('访问出现异常。');
+}
+
+if(!isset ($_POST['status'])){
+    die('访问出现异常。');
+}
+
+$status = $_POST['status'];
 $itemData = $_POST['itemData'];
-var_dump($itemData);
-print_r($itemData);
+
+foreach ($itemData as $key => $value) {
+    if (checkInput($value)) {
+        die('输入的内容不能包含字符 ：' . checkInput($value));
+    }
+}
