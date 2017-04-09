4524b9600fea83a7ac7bfe259f2893a37e715122
Felix <jadeshaw@163.com>
2017-04-09 18:16:56 +0800

    注销登陆，释放用户登录信息

--- app/login/logOff.php
+++ app/login/logOff.php
@@ -2,7 +2,7 @@
 //注销session
 session_start();
 unset($_SESSION['username']);
-unset($_SESSION['password']);
+unset($_SESSION['nick_name']);
 unset($_SESSION['role_group']);
 unset($_SESSION['uid']);
 echo "<script language='javascript' type='text/javascript'> 
