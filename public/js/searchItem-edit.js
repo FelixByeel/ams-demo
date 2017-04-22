0e726dc0ecb493a13dd551b32fdf038857bc195e
Felix <jadeshaw@163.com>
2017-04-22 18:58:49 +0800

    修改编辑按钮id为class，去掉多余注释

--- public/js/searchItem-edit.js
+++ public/js/searchItem-edit.js
@@ -7,13 +7,7 @@ function showCurrentSelectedDetail(itemJSON, currentSelectedId = 0) {
 
     tableObj.className = "show-detail-table";
     trObj.className = "tr-title";
-    /*
-    trObj.insertCell(0).innerHTML = "分类编号";
-    trObj.insertCell(1).innerHTML = "分类名称";
-    trObj.insertCell(2).innerHTML = "所属仓库";
-    trObj.insertCell(3).innerHTML = "数量";
-    trObj.insertCell(4).innerHTML = "操作";
-    */
+
     let trStr = "<td class = 'code-column td-title'>编号</td>";
     trStr += "<td class = 'name-column td-title'>名称</td>";
     trStr += "<td class = 'warehouse-column td-title'>仓库</td>";
@@ -24,8 +18,6 @@ function showCurrentSelectedDetail(itemJSON, currentSelectedId = 0) {
 
     for (let i = 0, j = 0; i < itemJSON.length; i++) {
 
-        //let flag = currentSelectedId ? ((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)) : (1 == itemJSON[i].is_ended);
-        //if ((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)) {
         //根据currentSelectedId 显示，为0表示默认显示所有物品条目，否则显示当前所选择的currentSelectedId的下的所有最终物品条目
         if(currentSelectedId ? ((currentSelectedId == itemJSON[i].parent_id) && (1 == itemJSON[i].is_ended)) : (1 == itemJSON[i].is_ended)){
             let trObj = tableObj.insertRow();
@@ -36,8 +28,6 @@ function showCurrentSelectedDetail(itemJSON, currentSelectedId = 0) {
             else {
                 trObj.className = "even-row";
             }
-            //trObj.insertCell(0).innerHTML = itemJSON[i].item_id;
-            //trObj.insertCell(1).innerHTML = itemJSON[i].item_name;
             trStr += "<td class = 'code-column td-content'>"+ itemJSON[i].item_id +"</td>";
             trStr += "<td class = 'name-column td-content'>"+ itemJSON[i].item_name +"</td>";
 
@@ -49,18 +39,13 @@ function showCurrentSelectedDetail(itemJSON, currentSelectedId = 0) {
             }
 
             if ('undefined' == typeof (warehouseJSON[k])) {
-                //trObj.insertCell(2).innerHTML = '无';
                 trStr += "<td class = 'warehouse-column td-content'>无</td>";
             }
             else {
-                //trObj.insertCell(2).innerHTML = warehouseJSON[k].warehouse_name;
                 trStr += "<td class = 'warehouse-column td-content'>"+ warehouseJSON[k].warehouse_name +"</td>";
             }
-
-            //trObj.insertCell(3).innerHTML = itemJSON[i].item_count;
-            //trObj.insertCell(4).innerHTML = "<button id = 'edit' onclick = 'editItem(" + itemJSON[i].id + ")'>编辑</button>";
             trStr += "<td class = 'count-column td-content'>"+ itemJSON[i].item_count +"</td>";
-            trStr += "<td class = 'action-column td-content'><button id = 'edit' onclick = 'editItem(" + itemJSON[i].id + ")'>编辑</button></td>";
+            trStr += "<td class = 'action-column td-content'><button class = 'edit-button' onclick = 'editItem(" + itemJSON[i].id + ")'>编辑</button></td>";
             trObj.innerHTML = trStr;
             j++;
         }
@@ -69,64 +54,6 @@ function showCurrentSelectedDetail(itemJSON, currentSelectedId = 0) {
     itemDetailDivObj.appendChild(tableObj);
 }
 
-/*
-//页面加载时显示详细信息
-function loadAllEndedItems(itemJSONFun) {
-    //console.log('f_' + itemJSONFun);
-    let itemDetailDivObj = document.getElementById("itemDetail");
-    let tableObj = document.createElement("table");
-    let trObj = tableObj.insertRow();
-
-    tableObj.id = "showDetailTable_allItems";
-    trObj.id = "trHead";
-    trObj.insertCell(0).innerHTML = "分类编号";
-    trObj.insertCell(1).innerHTML = "分类名称";
-    trObj.insertCell(2).innerHTML = "所属仓库";
-    trObj.insertCell(3).innerHTML = "数量";
-    trObj.insertCell(4).innerHTML = "操作";
-
-    for (let i = 0, j = 0; i < itemJSONFun.length; i++) {
-
-        if (1 == itemJSONFun[i].is_ended) {
-
-            let trObj = tableObj.insertRow();
-            if (j % 2) {
-                trObj.className = "row_odd";
-            }
-            else {
-                trObj.className = "row_even";
-            }
-            trObj.insertCell(0).innerHTML = itemJSONFun[i].item_id;
-            trObj.insertCell(1).innerHTML = itemJSONFun[i].item_name;
-
-
-
-            let k = 0;
-            for (; k < warehouseJSON.length; k++) {
-
-                if (warehouseJSON[k].warehouse_id == itemJSONFun[i].warehouse_id) {
-
-                    break;
-                }
-            }
-
-            if ('undefined' == typeof (warehouseJSON[k])) {
-                trObj.insertCell(2).innerHTML = '无';
-            }
-            else {
-                trObj.insertCell(2).innerHTML = warehouseJSON[k].warehouse_name;
-            }
-
-            trObj.insertCell(3).innerHTML = itemJSONFun[i].item_count;
-            trObj.insertCell(4).innerHTML = "<button id = 'edit' onclick = 'editItem(" + itemJSONFun[i].id + ")'>编辑</button>";
-
-            j++;
-        }
-    }
-    itemDetailDivObj.innerHTML = "";
-    itemDetailDivObj.appendChild(tableObj);
-}
-*/
 //---------------编辑操作 start--------------------
 function editItem(currentSelectedId) {
 
@@ -252,14 +179,6 @@ $("#saveButton").click(function () {
     $("#shadeBox").height("0");
     document.documentElement.style.overflowY = "scroll";
 });
-
-/*
-//取消
-$("#cancelButton").click(function () {
-
-    $("#editBox").hide();
-});
-*/
 
 //点击弹出窗口右上角X
 function closePopLayer(){
