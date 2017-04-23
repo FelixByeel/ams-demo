b13c7b9c91ff998ae62474cf3c23f69bb4745ca2
Felix <jadeshaw@163.com>
2017-04-23 09:44:06 +0800

    调整record_t表字段

--- data/db_script/create.sql
+++ data/db_script/create.sql
@@ -39,7 +39,8 @@ create table record_t(
     update_count int(10)  not null default 0 comment '本次变更数量',
     consumer_code varchar(15) not null default '' comment '物品使用人工号',
     computer_barcode varchar(15) not null default '' comment '电脑资产条码',
-    item_sn_code varchar(15) not null default '' comment '物品序列号编码',
+    item_sn varchar(15) not null default '' comment '物品序列号编码',
+    computer_sn varchar(15) not null default '' comment '电脑序列号',
     username varchar(20) not null default '' comment '当前操作人，对应用户信息表username字段',
     primary key(id)
 )engine=innodb default charset=utf8 comment='出入库记录表';
