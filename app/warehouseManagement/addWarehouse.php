22dd76f2c8e9de3daa316bff276e053be0a0db08
Felix <jadeshaw@163.com>
2017-04-25 22:29:29 +0800

    样式调整，修改添加仓库弹出窗口位置和样式统一

--- app/warehouseManagement/addWarehouse.php
+++ app/warehouseManagement/addWarehouse.php
@@ -35,8 +35,8 @@
             </div>
 
             <!--弹出层-->
-            <div id = "editWarehousePopLayer" style="display:none">
-                <span id = "closeSpan" onclick="closePopLayer()">&times;</span>
+            <div id = "editWarehousePopLayer" class = "poplayer" style="display:none">
+                <span class = 'popLayerTitle'>添加仓库<span class = "close-poplayer" onclick="closePopLayer()">&times;</span></span>
                 <div id = "actionEditBox">
                     <label id = "warehouseCodeP" style="display:none">仓库编号：<span id = "warehouseCodeSpan"></span></label>
                     <label id = "warehouseCodeP">仓库名称：
