ba8b565421100245f12db2b6af1a92d52ae41830
Felix <jadeshaw@163.com>
2017-05-08 17:23:19 +0800

    处理查询

--- app/recordManagement/getSearchRecordService.php
+++ app/recordManagement/getSearchRecordService.php
@@ -1,2 +1,26 @@
 <?php
-echo "2";
+//本页面处理记录查询
+define('APP_ROOT', dirname(dirname(__DIR__)).'/');
+
+//加载用户登陆验证
+require_once (APP_ROOT.'app/login/loginCheck.php');
+
+//加载数据验证
+require_once (APP_ROOT.'include/checkInput.php');
+
+//加载数据库配置
+require_once (APP_ROOT.'include/dbConfig.php');
+require_once (APP_ROOT.'include/Msqli.class.php');
+
+if(isset($_POST['searchConditions'])) {
+    $searchConditions = $_POST['searchConditions'];
+    foreach($searchConditions as $key => $value) {
+        if ($checkChar = checkInput($value)) {
+            die ('输入的内容不能包含：' + $checkChar);
+        }
+    }
+}else {
+    $searchConditions = '';
+}
+
+
