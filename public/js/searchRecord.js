8ab93da7377fe23cbb93bce643c51712ffbe7aa0
Felix <jadeshaw@163.com>
2017-05-08 16:38:02 +0800

    处理输入的查询条件

--- public/js/searchRecord.js
+++ public/js/searchRecord.js
@@ -1,6 +1,19 @@
 //searchRecord
+
+//页面加载
+window.onload = function (){
+    var recordCount = getRecordCount();
+}
+
+//点击搜索按钮
 function searchRecord() {
 
+    var searchConditions = getInputConditions();
+    console.log(searchConditions);
+}
+
+//获取输入的查询条件
+function getInputConditions() {
     //获取输入的日期并转为时间戳
     var startTimeStr = $("#startDate").val();
     var endTimeStr = $("#endDate").val();
@@ -11,18 +24,56 @@ function searchRecord() {
     var dealType = $("#dealType option:selected").val();
     //用户工号
     var consumerCode = $("#consumerCode").val();
-        if(str = checkInputStr.isExistSpecialChar(consumerCode)) {
-        alert("输入内容中不能含有：" + str);
+    if (!consumerCode.length) {
+        consumerCode = '';
+    }
+    else if (str = checkInputStr.isExistSpecialChar(consumerCode)) {
+        alert("用户工号中不能含有：" + str);
         $("#consumerCode").focus();
         return false;
     }
     //资产条码
     var computerBarcode = $("#computerBarcode").val();
+    if (!computerBarcode.length) {
+        computerBarcode = '';
+    }
+    else if (!checkInputStr.isDigital(computerBarcode)) {
+        alert("资产条码只能包含数字！");
+        $("#computerBarcode").focus();
+        return false;
+    }
     //物品序列号
     var itemSN = $("#itemSN").val();
-    //处理人
+    if (!itemSN.length) {
+        itemSN = '';
+    }
+    else if (str = checkInputStr.isExistSpecialChar(itemSN)) {
+        alert("物品序列号中不能含有：" + str);
+        $("#itemSN").focus();
+        return false;
+    }
+    //处理人工号
     var username = $("#username").val();
+    if (!username.length) {
+        username = '';
+    }
+    else if (str = checkInputStr.isExistSpecialChar(username)) {
+        alert("处理人工号中不能含有：" + str);
+        $("#username").focus();
+        return false;
+    }
 
+    var searchConditionsData = {
+        "startTime": startTime,
+        "endTime": endTime,
+        "dealType": dealType,
+        "consumerCode": consumerCode,
+        "computerBarcode": computerBarcode,
+        "itemSN": itemSN,
+        "username": username
+    }
+
+    return searchConditionsData;
 }
 
 //将日期格式字符串转为时间戳，如“2017-5-2 12:00:00”转为:1493697600,源字符串为空或格式不正确，返回0;
@@ -31,4 +82,22 @@ function stringParseToTimestamp(timeStr) {
     var timestamp = Date.parse(new Date(timeStr));
     timestamp = timestamp / 1000;                       //将上一步获取的毫秒格式时间戳转为以秒记的时间戳。
     return isNaN(timestamp) ? 0 : timestamp;
+}
+
+//获取总记录条数
+function getRecordCount(){
+    var recordCount;
+    $.ajax({
+        type: "get",
+        url: "getRecordCountService.php",
+        async : false,
+        cache: false,
+        success: function (data) {
+            recordCount = data;
+        },
+        error: function (msg, e) {
+            alert("请求的数据发生异常：" + e);
+        }
+    });
+    return recordCount;
 }
