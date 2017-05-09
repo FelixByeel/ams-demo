e953ea2494889fdf4750cf891c3938376a15bc5c
Felix <jadeshaw@163.com>
2017-05-09 16:40:28 +0800

    修改空格字符检测的提示，添加去除空格function

--- public/js/checkInputStr.js
+++ public/js/checkInputStr.js
@@ -5,6 +5,11 @@
  */
 var checkInputStr = {
 
+    //去除首尾所有空格
+    trimSpace : function (str) {
+        return str.replace(/(^\s*)|(\s*$)/g, "");
+    },
+
     //是否为由数字组成的字符串，数字返回true,含有非数字返回false。
     isDigital : function (str) {
         var reg=/^[0-9]*$/;//匹配整数
@@ -20,7 +25,11 @@ var checkInputStr = {
 
         for(var i = 0; i < specialCharacter.length; i++) {
             if(str.indexOf(specialCharacter[i]) != -1) {
-                return specialCharacter[i];
+                if(' ' == specialCharacter[i] || '　'　== specialCharacter[i]) {
+                    return "空格";
+                }else {
+                    return specialCharacter[i];
+                }
             }
         }
         return false;
