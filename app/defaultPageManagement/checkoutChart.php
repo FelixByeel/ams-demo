bafd8ce4a443f0bf365f83bc0e93666f4fd26430
Felix <jadeshaw@163.com>
2017-05-29 18:45:48 +0800

    查询所有分类

--- app/defaultPageManagement/checkoutChart.php
+++ app/defaultPageManagement/checkoutChart.php
@@ -1,3 +1,22 @@
 <?php
+define('APP_ROOT', dirname(dirname(__DIR__)).'/');
+
+//加载用户登陆验证
+require_once (APP_ROOT.'app/login/loginCheck.php');
+
+//加载数据验证
+require_once (APP_ROOT.'include/checkInput.php');
+
+//加载数据库配置
+require_once (APP_ROOT.'include/dbConfig.php');
+require_once (APP_ROOT.'include/Msqli.class.php');
+
+//测试输出数据
 echo $_POST['data'] . '<br/>';
-echo date("Y-m-d H:i:s T", time());
+
+//连接数据库
+$mysqli         = new Msqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
+
+$tableName = 'item_t';
+$columnArray = array('id');
+$conditionStr = ' is_ended = 1';
