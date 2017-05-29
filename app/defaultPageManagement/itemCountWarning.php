ec65a66c8120d112abaee46d7b766a0fe0edbaab
Felix <jadeshaw@163.com>
2017-05-29 18:48:01 +0800

    修改查询逻辑

--- app/defaultPageManagement/itemCountWarning.php
+++ app/defaultPageManagement/itemCountWarning.php
@@ -16,19 +16,19 @@ require_once (APP_ROOT.'include/Msqli.class.php');
 $mysqli         = new Msqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 $tableName      = 'item_t';
 $columnArray    = array('item_name', 'item_count');
-$conditionStr   = ' is_ended = 1 order by item_count asc';
+$conditionStr   = ' is_ended = 1 and item_count < 5 order by item_count asc';
 
 $result = $mysqli->select($tableName, $columnArray, $conditionStr);
 
 $str = '<table class = \'item-warning-table\'>';
 
 while ($row = mysqli_fetch_assoc($result)) {
-    if($row['item_count'] < 5) {
+    //if($row['item_count'] < 5) {
         $str .= '<tr class = \'item-tr\'>';
         $str .= '<td class = \'item-td\'>' . $row['item_name'] . '</td>';
         $str .= '<td class = \'item-td\'>库存剩余数量：' . $row['item_count'] . '</td>';
         $str .= '</tr>';
-    }
+    //}
 }
 
 $str .= '</table>';
