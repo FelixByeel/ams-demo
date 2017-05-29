c1432aedd725e7a765d66edc453753c028041749
Felix <jadeshaw@163.com>
2017-05-29 19:05:29 +0800

    修改未登录提示

--- app/login/loginCheck.php
+++ app/login/loginCheck.php
@@ -3,5 +3,12 @@
 session_start();
 
 if(!isset($_SESSION['uid']) || empty($_SESSION['uid'])){
-    die("请登陆后操作！");
+    die("<span>请登陆后操作！3秒后自动跳转。</span><a href = '/'>未跳转？点击我。</a>
+    <script language='javascript' type='text/javascript'>
+        var timer = setInterval('goToLogin()', 3000);
+            function goToLogin () {
+                window.location.href='/';
+            }
+    </script>
+    ");
 }
