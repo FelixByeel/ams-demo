2a97d197cfba56e581df8e6b5f5598a6c524e6e6
Felix <jadeshaw@163.com>
2017-05-30 22:54:04 +0800

    测试数据

--- app/defaultPageManagement/checkoutChart.php
+++ app/defaultPageManagement/checkoutChart.php
@@ -18,5 +18,20 @@ echo $_POST['data'] . '<br/>';
 $mysqli         = new Msqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
 
 $tableName = 'item_t';
-$columnArray = array('id');
-$conditionStr = ' is_ended = 1';
+$columnArray = array('id', 'item_name');
+$conditionStr = ' is_ended = 1 order by CONVERT(item_name USING gbk)';
+
+$result = $mysqli->select($tableName, $columnArray, $conditionStr);
+
+$itemName = array();
+echo "<select>";
+while ($row = mysqli_fetch_assoc($result)) {
+    $itemName[] = $row['item_name'];
+
+    echo "<option value = '{$row['id']}'>{$row['item_name']}</option>";
+
+}
+echo "</select>";
+var_dump($itemName);
+
+
