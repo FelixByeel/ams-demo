23b417fd49ed3023700d3c2f47d18722e877116b
Felix <jadeshaw@163.com>
2017-05-31 16:34:28 +0800

    修改未登录状态跳转到登陆页

--- app/login/loginCheck.php
+++ app/login/loginCheck.php
@@ -3,11 +3,11 @@
 session_start();
 
 if(!isset($_SESSION['uid']) || empty($_SESSION['uid'])){
-    die("<span>请登陆后操作！3秒后自动跳转。</span><a href = '/'>未跳转？点击我。</a>
+    die("<span>请登陆后操作！3秒后自动跳转。</span><a href = 'index.php'>未跳转？点击我。</a>
     <script language='javascript' type='text/javascript'>
         var timer = setInterval('goToLogin()', 3000);
             function goToLogin () {
-                window.location.href='/';
+                window.location.href='index.php';
             }
     </script>
     ");
