1aefda32fce6251c15f15e88e69dfd268957a24a
Felix <jadeshaw@163.com>
2017-06-04 13:39:50 +0800

    获取统计数据

--- app/defaultPageManagement/checkoutChart.php
+++ app/defaultPageManagement/checkoutChart.php
@@ -15,7 +15,7 @@ require_once (APP_ROOT.'include/Msqli.class.php');
 $windowScreenHeight     = isset($_POST['data']['windowScreenHeight']) ? $_POST['data']['windowScreenHeight'] : 768;
 //当前select选择项
 $itemSelectValue        = isset($_POST['data']['itemSelectValue']) ? $_POST['data']['itemSelectValue'] : 0;
-
+//检查提交的数据合法性
 if (0 != $itemSelectValue && !preg_match('/^[1-9][0-9]*$/', $itemSelectValue)) {
     die('提交的数据异常');
 }
@@ -35,7 +35,7 @@ $itemArr        = array();
 $htmlStr        = "<select id = 'itemSelect' class = 'chart-select'>";
 
 while ($row = mysqli_fetch_assoc($result)) {
-    $itemArr[$row['id']] = $row['item_name'];
+    $itemArr[] = $row['id'];
     if ($itemSelectValue == $row['id']) {
         $htmlStr .= "<option value = '{$row['id']}' selected = 'selected'>{$row['item_name']}</option>";
     } else {
@@ -45,12 +45,12 @@ while ($row = mysqli_fetch_assoc($result)) {
 $htmlStr .= "</select>";
 echo $htmlStr;
 
-//查询出库记录并统计数据
-$tableName      = 'record_t';
-$columnArray    = array('record_time', 'update_count');
-$conditionStr   = " item_id = {$itemSelectValue} and record_status = '出库'";
+/**
+*统计出库数据。
+*根据当前所选择的项，统计最近5个月的出库数量。例如 笔记本硬盘500G， 2017-6 出库数量为 1 ， 2017-5 出库数量为 2
+*/
 
-//保存最近5个月的月份
+//获取最近5个月的月份
 $dateArr = array();
 for ($i = 0; $i < 5; $i++) {
     if (!$i) {
@@ -60,21 +60,47 @@ for ($i = 0; $i < 5; $i++) {
     }
 }
 
-echo '<br/>';
-var_dump($dateArr);
-echo '<br/>';
+//查询出库记录并统计数据
+$tableName      = 'record_t';
+$columnArray    = array('record_time', 'update_count');
 
-$itemCount  = 0;    //保存当前所选项的总出库数量
-$result     = $mysqli->select($tableName, $columnArray, $conditionStr);
-while ($row = mysqli_fetch_assoc($result)) {
-    $itemCount += $row['update_count'];
+//查询条件
+//第一次页面加载，默认查询第一项,并查询最近{$length}个月的数据。
+if (!$itemSelectValue) {
+    $itemSelectValue = $itemArr[0];
 }
-//测试输出数据
-var_dump($_POST['data']) . '<br/>';
+$length = count($dateArr);
+$startDate  = strtotime($dateArr[$length - 1]);
+$conditionStr   = " item_id = {$itemSelectValue} and record_time > {$startDate} and record_status = '出库'";
 
-echo $itemCount;
-echo '<br/>';
+//查询结果集
+$result     = $mysqli->select($tableName, $columnArray, $conditionStr);
 
+//初始化以日期为key的统计数组，将查询结果以月份进行统计。
+//$itemCount['2017-6'] = 0;
+//$itemCount['2017-5'] = 0;
+//$itemCount['2017-4'] = 0;
+//...
+foreach ($dateArr as $key => $value) {
+    $itemCount[$value] = 0;
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    for ($i = 0; $i < count($dateArr); $i++) {
+        $startDate = strtotime($dateArr[$i]);
+        $endDate = $i ? strtotime($dateArr[$i - 1]) : time();
+        if ($row['record_time'] > $startDate && $row['record_time'] < $endDate) {
+            $itemCount[$dateArr[$i]] += $row['update_count'];
+        }
+    }
+}
+
+//测试数据
+foreach ($itemCount as $key => $value) {
+    echo '<br/>';
+    echo $key . ' : ' . $value;
+    echo '<br/>';
+}
 
 //根据当前月份获取上月
 function getLastMonth($dateStr)
@@ -90,3 +116,5 @@ function getLastMonth($dateStr)
     }
     return $year . '-' . $month;
 }
+
+//将上面获得的统计数据，以图表展示。此处为折线图。
