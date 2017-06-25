7512a54e76ba66db53878c8434d553a7a91de141
Felix <jadeshaw@163.com>
2017-06-25 17:19:29 +0800

    [BUG]修复无法正确跳转登录页面问题

--- main.php
+++ main.php
@@ -1,5 +1,17 @@
 <?php
-    require_once('app/login/loginCheck.php');
+//登录验证
+session_start();
+
+if(!isset($_SESSION['uid']) || empty($_SESSION['uid'])){
+    die("<span>请登陆后操作！3秒后自动跳转。</span><a href = 'index.php'>未跳转？点击这里。</a>
+    <script language='javascript' type='text/javascript'>
+        var timer = setInterval('goToLogin()', 3000);
+        function goToLogin () {
+            window.location.href='index.php';
+        }
+    </script>
+    ");
+}
 ?>
 <!DOCTYPE>
 <html>
