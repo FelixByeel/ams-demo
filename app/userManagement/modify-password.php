b25ca76c378f9c2d40bdf67d92c92d2fa16d3c4e
Felix <jadeshaw@163.com>
2017-06-25 12:46:06 +0800

    [ADD]Add css file

--- app/userManagement/modify-password.php
+++ app/userManagement/modify-password.php
@@ -8,15 +8,20 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="../../public/css/modify-password.css" rel="stylesheet">
     </head>
     <body>
         <div class = "modify-password-box">
-            <label>用户名：<span class = "username-span"><?php echo $_SESSION['username']; ?></span></label>
-            <label>请输入原密码：<input class = "old-pwd-input" type = "password" maxlength = "15"/></label>
-            <label>请输入新密码：<input class = "new-pwd-input" type = "password" maxlength = "15"/></label>
-            <label>请确认新密码：<input class = "cfm-pwd-input" type = "password" maxlength = "15"/></label>
-            <button id = "savePwdBtn" class = "save-pwd-btn">确认</button>
+            <div class = "pwd-box">
+                <label>用户名：<span class = "username-span"><?php echo $_SESSION['username']; ?></span></label>
+                <label>请输入原密码：<input class = "old-pwd-input" type = "password" maxlength = "15"/></label>
+                <label>请输入新密码：<input class = "new-pwd-input" type = "password" maxlength = "15"/></label>
+                <label>请确认新密码：<input class = "cfm-pwd-input" type = "password" maxlength = "15"/></label>
+            </div>
+            <div class = "btn-box">
+                <button id = "savePwdBtn" class = "save-pwd-btn">保&nbsp;&nbsp;存</button>
+            </div>
+
         </div>
     </body>
 </html>
